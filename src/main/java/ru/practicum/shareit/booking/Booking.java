9f2a63d77c4d47d4abe8d45d15c2e607e6fa8c1f
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-12-13 13:17:14 +0700

    rebuild sql options

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -38,7 +38,7 @@ public class Booking {
     @Column(name = "start_date")
     private LocalDateTime start;
 
-    @Column(name = "end_time")
+    @Column(name = "end_date")
     private LocalDateTime end;
 
     @ManyToOne(cascade = CascadeType.ALL)
